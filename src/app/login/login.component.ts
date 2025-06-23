66b62113a244635b35c304f7112d4a66be520eba
Johnny Loachamin <Jsloachamin1@espe.edu.ec>
2025-06-23 15:00:51 -0500

    Add loading screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,24 +2,28 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { LoginRequest } from '../models/auth.model';
-import { FormsModule } from '@angular/forms'; // Importar FormsModule
-import { CommonModule } from '@angular/common'; // Importar CommonModule
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule] // Asegurar que imports sea un array
+  imports: [FormsModule, CommonModule]
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
   errorMessage: string = '';
+  isLoading: boolean = false; // Bandera para controlar el modal de carga
 
   constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit(): void {
+    this.isLoading = true; // Mostrar modal
+    this.errorMessage = ''; // Limpiar mensaje de error
+
     const credentials: LoginRequest = {
       nombreUsuario: this.username,
       contrasena: this.password
@@ -27,9 +31,11 @@ export class LoginComponent {
 
     this.authService.login(credentials).subscribe({
       next: () => {
+        this.isLoading = false; // Ocultar modal
         this.router.navigate(['']);
       },
       error: (err) => {
+        this.isLoading = false; // Ocultar modal
         this.errorMessage = 'Usuario o contraseña incorrectos. Por favor, intenta de nuevo.';
         console.error('Error en el login:', err);
       }
